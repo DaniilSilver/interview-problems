8036d81312dff0b46a417198a2968bfd5584d46c
Daniil Silver <148889410+DaniilSilver@users.noreply.github.com>
2023-10-24 22:51:42 +0300

    first exercise

--- src/java_init_ex1.java
+++ src/java_init_ex1.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 public class java_init_ex1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Input string: ");
+        String st = sc.nextLine();
+        System.out.println("Input n: ");
+        int n = sc.nextInt();
+        System.out.println();
+        if (n<=1) System.out.println(st);
+        else {
+            for (int i=0;i<=n;i++){
+                System.out.println(st);
+            }
+        }
+        sc.close();
+
+
 
 
     }
